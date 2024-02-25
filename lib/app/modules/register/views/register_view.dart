047674ce536e28524d60210a759b09904a0881a4
eganaww <geminggg81@gmail.com>
2024-02-25 16:28:49 +0700

    Login View

--- lib/app/modules/register/views/register_view.dart
+++ lib/app/modules/register/views/register_view.dart
@@ -1,78 +1,104 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/register_controller.dart';
 
 class RegisterView extends GetView<RegisterController> {
   const RegisterView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('RegisterView'),
+        title: const Text('Daftar Akun'),
         centerTitle: true,
       ),
       body: Center(
-        child: Form(
-          key: controller.formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextFormField(
-                controller: controller.namaController,
-                decoration: InputDecoration(hintText: "Masukkan Nama Lengkap"),
-                validator: (value){
-                  if (value!.isEmpty){
-                    return "Nama Lengkap tidak boleh kosong";
-                  }
-                  return null;
-                },
+              Image.asset(
+                'assets/img/Logo-apk.png', // Ganti dengan path menuju gambar logo
+                width: 200, // Sesuaikan dengan ukuran yang diinginkan
+                height: 200,
               ),
-              TextFormField(
-                controller: controller.usernameController,
-                decoration: InputDecoration(hintText: "Masukkan Username"),
-                validator: (value){
-                  if (value!.isEmpty){
-                    return "Username tidak boleh kosong";
-                  }
-                  return null;
-                },
+              SizedBox(height: 20),
+              Form(
+                key: controller.formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: controller.namaController,
+                      decoration: InputDecoration(hintText: "Masukkan Nama Lengkap"),
+                      validator: (value){
+                        if (value!.isEmpty){
+                          return "Nama Lengkap tidak boleh kosong";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: controller.usernameController,
+                      decoration: InputDecoration(hintText: "Masukkan Username"),
+                      validator: (value){
+                        if (value!.isEmpty){
+                          return "Username tidak boleh kosong";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: controller.telpController,
+                      decoration: InputDecoration(hintText: "Masukkan Email"),
+                      validator: (value){
+                        if (value!.isEmpty){
+                          return "Nomor Telpon tidak boleh kosong";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: controller.alamatController,
+                      decoration: InputDecoration(hintText: "Masukkan Alamat Rumah"),
+                      validator: (value){
+                        if (value!.isEmpty){
+                          return "Alamat Rumah tidak boleh kosong";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: controller.passwordController,
+                      decoration: InputDecoration(hintText: "Masukkan Password"),
+                      validator: (value){
+                        if (value!.isEmpty){
+                          return "Password tidak boleh kosong";
+                        }
+                        return null;
+                      },
+                    ),
+                    Obx(() => controller.loading.value?
+                    CircularProgressIndicator():
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ElevatedButton(
+                        onPressed: (){
+                          controller.post();
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.green.shade800),
+                        ),
+                        child: Text("Daftar", style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        )),
+                      ),
+                    ),
+                    ),
+                  ],
+                ),
               ),
-              TextFormField(
-                controller: controller.telpController,
-                decoration: InputDecoration(hintText: "Masukkan Nomor Telepon"),
-                validator: (value){
-                  if (value!.isEmpty){
-                    return "Nomor Telpon tidak boleh kosong";
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: controller.alamatController,
-                decoration: InputDecoration(hintText: "Masukkan Alamat Rumah"),
-                validator: (value){
-                  if (value!.isEmpty){
-                    return "Alamat Rumah tidak boleh kosong";
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: controller.passwordController,
-                decoration: InputDecoration(hintText: "Masukkan Password"),
-                validator: (value){
-                  if (value!.isEmpty){
-                    return "Password tidak boleh kosong";
-                  }
-                  return null;
-                },
-              ),
-              Obx(() => controller.loading.value?
-              CircularProgressIndicator():
-              ElevatedButton(onPressed: (){
-                controller.post();
-              }, child: Text("Daftar")))
             ],
           ),
         ),
